8f0b44984f68a79b3cc3cc3ebfcfe486617db258
Richa Juvekar <richa.juvekar@gmail.com>
2020-10-23 20:40:28 -0400

    Magic Square isMagic done

--- MagicSquare/src/com/richa/MagicSquare.java
+++ MagicSquare/src/com/richa/MagicSquare.java
@@ -2,12 +2,12 @@ package com.richa;
 
 public class MagicSquare {
 
-    int dimensions = 0;
+    int dimension = 0;
     int[][] magicSquare = null;
 
     MagicSquare(int d) {
 
-        dimensions = d;
+        dimension = d;
         magicSquare = new int[d][d];
 
         for (int i = 0; i < d; i++) {
@@ -22,7 +22,52 @@ public class MagicSquare {
         magicSquare[i][j] = number;
     }
 
-    public boolean isMagic
+    public int isMagic() {
+        int constant = -1;
+
+        // compare all the rows
+        for (int i = 0; i < dimension; i++) {
+            int sumRow = 0;
+            for (int j = 0; j < dimension; j++) {
+                sumRow += magicSquare[i][j];
+            }
+            if (constant == -1) {
+                constant = sumRow;
+            } else {
+                if (sumRow != constant){
+                    return 0;
+                }
+            }
+        }
+        // compare all the columns
+        for (int i = 0; i < dimension; i++) {
+            int sumColumns = 0;
+            for (int j = 0; j < dimension; j++) {
+                sumColumns += magicSquare[j][i];
+            }
+            if (sumColumns != constant){
+                return 0;
+            }
+        }
+        // compare the diagonals
+        int sumDiag1 = 0;
+        for (int i = 0; i < dimension; i++) {
+            sumDiag1 += magicSquare[i][i];
+        }
+        if (sumDiag1 != constant){
+            return 0;
+        }
+        int sumDiag2 = 0;
+        for (int i = 0; i < dimension; i++) {
+            sumDiag2 += magicSquare[i][dimension - i - 1];
+        }
+        if (sumDiag2 != constant){
+            return 0;
+        }
+
+        // all conditions for a magic square are met
+        return constant;
+    }
 
 }
 
--- MagicSquare/src/com/richa/Main.java
+++ MagicSquare/src/com/richa/Main.java
@@ -13,18 +13,25 @@ public class Main {
             int dimension = scanner.nextInt();
 
             if (2 <= dimension && dimension <= 8) {
+
                 dimProvided = true;
                 MagicSquare ms = new MagicSquare(dimension);
+
                 for (int i = 0; i < dimension; i++) {
                     for (int j = 0; j < dimension; j++) {
-                        System.out.print("Enter a number to fill in the magic square: ");
+                        System.out.print("Enter a number at [" + i + "," + j + "]: ");
                         int userNumInput = scanner.nextInt();
                         ms.addNumber(i, j, userNumInput);
                     }
                 }
 
 
-
+                int constant = ms.isMagic();
+                if (constant == 0) {
+                    System.out.println("This is not a magic square.");
+                } else {
+                    System.out.println("This is a magic square with a constant of " + constant + ".");
+                }
 
             } else {
                 System.out.println("Dimensions for the magic square have to be between 2 and 8.");
